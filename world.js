1b127a60940215ab25d70449599291bd4238430a
CaseyWillis1 <caseywillis140@gmail.com>
2021-11-07 18:16:45 -0600

    Edited javascript file

--- world.js
+++ world.js
@@ -4,19 +4,45 @@
 
 "use strict";
 
-document.addEventListener('DOMContentLoaded', () =>{
-  let button = document.querySelector('button');
- 
-  button.addEventListener('click', () => lookup());
- 
-  function lookup(){
 
-  let xhr = new XMLHttpRequest();
+document.addEventListener('DOMContentLoaded', () =>{
+    let button = document.getElementById('country2');
+ 
+    button.addEventListener('click', () => lookup());
+ 
+    function lookup(){
+      let country = document.querySelector('#country').value;
+
+      let xhr = new XMLHttpRequest();
   
-  let lookup = document.getElementById('country').value;
+      xhr.open('GET',`world.php?country=${country}&context=country`);
+      console.log(country);
     
-    xhr.open('GET',`world.php?country=${lookup}`);
+
+      xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+          if(xhr.status == 200){ 
+            document.getElementById("result").innerHTML = xhr.responseText; 
+          }
+        }
+      }
+      xhr.send();
+    }
+  
+
+    let button2 = document.getElementById('cities');
+ 
+    button2.addEventListener('click', () => lookup2());
+ 
+    function lookup2(){
+
+    let country = document.querySelector('#country').value;
+    let xhr = new XMLHttpRequest();
+ 
+    xhr.open('GET',`world.php?country=${country}&context=cities`);
+    console.log(country);
     
+
     xhr.onreadystatechange = function(){
       if(xhr.readyState == 4){
         if(xhr.status == 200){ 
